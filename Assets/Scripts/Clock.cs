70b08864584b4eb5740cfedeb7b016757ff3f6f8
zerolizhi <657209613@qq.com>
2021-04-25 20:00:17 +0800

    unity practise

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    const float degreesPerHour = 30.0f;
+    const float degreesPerMinute = 6.0f;
+    const float degreesPerSecond = 6.0f;
+    public bool continous;
+    void Update()
+    {
+        if(continous)
+        {
+            UpdateContinous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+    void UpdateContinous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        //Debug.Log(DateTime.Now);
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours*degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+    void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        //Debug.Log(DateTime.Now);
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+    public GraphFunctionName function;
+    Transform[] points;
+    float spendTime;
+    [Range(10, 100)]
+    public int resolution = 10;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.z = 0f;
+        points = new Transform[resolution];
+        for(int i=0; i< points.Length; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = ((i + 0.5f) * step - 1f);
+            position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+        }
+    }
+    static float SineFunction(float x, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    static float MultiSineFunction(float x, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f*Mathf.PI * (x + t))/2f;
+        return y;
+    }
+
+    void Update()
+    {
+        float t = Time.time;
+        GraphFunction[] functions = {
+            SineFunction, MultiSineFunction
+        };
+        
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            
+            GraphFunction f = functions[(int)function];
+            position.y = f(position.x, t);
+            point.localPosition = position;
+        }
+    }
+}
